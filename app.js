e0653f102158f5c80b918fe488881ba1af09ad95
boneyardjed <jared.kovach@gmail.com>
2024-11-01 13:02:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const button2 = document.getElementById("button2");
     const backgroundMusic = document.getElementById("background-music");
     
-    const sound1 = new Audio("sound1.mp3");
-    const sound2 = new Audio("sound2.mp3");
+    const sound1 = new Audio("hey.m4a");
+    const sound2 = new Audio("ho.m4a");
 
     function flashBackground(color) {
         document.body.style.backgroundColor = color;
